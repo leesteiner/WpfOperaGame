1502652f3499f719b806e3b4f21afee2582dd0cf
Lee Steiner <leealansteiner@gmail.com>
2018-06-05 16:25:12 -0400

    Initial Commit

---
+++ WpfOperaGame/MainGameWindow.xaml.cs
@@ -0,0 +1,42 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Shapes;
+
+namespace WpfOperaGame
+{
+    /// <summary>
+    /// Interaction logic for MainGameWindow.xaml
+    /// </summary>
+    public partial class MainGameWindow : Window
+    {
+        public DateTime gameDate { get; set; } = DateTime.Now;
+
+        public MainGameWindow()
+        {
+            InitializeComponent();
+        }
+
+      
+        
+
+        private void Window_Loaded(object sender, RoutedEventArgs e)
+        {
+            PopulateFields();
+        }
+
+        private void PopulateFields()
+        {
+            gameDate = DateTime.Now;
+        }
+    }
+}
---
+++ WpfOperaGame/StartWindow.xaml.cs
@@ -0,0 +1,59 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+
+namespace WpfOperaGame
+{
+    /// <summary>
+    /// Interaction logic for MainWindow.xaml
+    /// </summary>
+    public partial class StartWindow : Window
+    {
+        public StartWindow()
+        {
+            InitializeComponent();
+        }
+
+        private void newGameButton_Click(object sender, RoutedEventArgs e)
+        {
+            NewGameWindow newGameWindow = new NewGameWindow();
+            this.Hide();
+            newGameWindow.ShowDialog();
+
+        }
+
+        private void loadGameButton_Click(object sender, RoutedEventArgs e)
+        {
+            LoadGameWindow loadGameWindow = new LoadGameWindow();
+            loadGameWindow.ShowDialog();
+        }
+
+        private void optionsButton_Click(object sender, RoutedEventArgs e)
+        {
+            OptionsWindow optionsWindow = new OptionsWindow();
+            optionsWindow.Show();
+        }
+
+        private void aboutButton_Click(object sender, RoutedEventArgs e)
+        {
+            AboutWindow aboutWindow = new AboutWindow();
+            aboutWindow.Show();
+        }
+
+        private void quitButton_Click(object sender, RoutedEventArgs e)
+        {
+            Application.Current.Shutdown();
+        }
+    }
+}
